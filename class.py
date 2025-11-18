7f490de58673eefcb1f793d0831914d9c25514a3
nika chitadze <nikachitadze13@gmail.com>
2025-11-18 22:10:43 +0400

    Update class.py

--- class.py
+++ class.py
@@ -3,28 +3,20 @@ class Person:
         self.name = name
         self.age = age
 
-    def __str__(self):
-        return f"Person: ({self.name}, {self.age})"
 def serialize(person):
     return f"Name: {person.name}, Age: {person.age}"
-
 def deserialize(text_line):
-    
-    parts = text_line.split(",")  
-    name_part = parts[0].split(":")[1].strip() 
-    age_part = parts[1].split(":")[1].strip()   
-
-    return Person(name_part, int(age_part))  
+    parts = text_line.split(",")   
+    name = parts[0].split(":")[1].strip()
+    age = int(parts[1].split(":")[1].strip())
+    return Person(name, age)
 
 p1 = Person("Otar", 34)
-serialized = serialize(p1)
 
-f = open("person.txt", "w")
-f.write(serialized)
-f.close()
-f = open("person.txt", "r")
-line = f.readline()
-f.close()
-new_person = deserialize(line)
+with open("person.txt", "w") as file:
+    file.write(serialize(p1))
 
-print(new_person)
+with open("person.txt", "r") as file:
+    line = file.readline()
+    restored = deserialize(line)
+print(f"Name: {restored.name}, Age: {restored.age}")
